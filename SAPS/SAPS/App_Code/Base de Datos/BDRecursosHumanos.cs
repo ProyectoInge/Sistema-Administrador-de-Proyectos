1145c69fba94c1b94238b129c6c0a89856d67cb4
SDelVecchioC <sdelvecchioc@gmail.com>
2015-10-06 12:20:46 +0800

    se agregó la clase BDRecursosHumanos y la conexión a la BD en amazon. Además se reorganizaron los archivos a la carpeta App_Code

---
+++ SAPS/SAPS/App_Code/Base de Datos/BDRecursosHumanos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAPS.App_Code.Base_de_Datos
+{
+    public class BDRecursosHumanos
+    {
+
+    }
+}
