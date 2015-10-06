91a01b3e2628c599acbe733d4c4b2973f8202347
SDelVecchioC <sdelvecchioc@gmail.com>
2015-10-06 12:32:50 +0800

    Se corrigió el nombre de App_Code a Codigo_Fuente

---
+++ SAPS/SAPS/Codigo_Fuente/Entidades/RecursoHumano.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Universidad de Costa Rica
+ * Escuela de Ciencias de la Computación e Informática
+ * Ingeniería de Software I
+ * Sistema Administrador de Proyectos de Software (SAPS)
+ * II Semestre 2015
+ * ---------------------------------------------------------------------------------------------
+ * Esta clase entidad se encarga de crear objetos "RecursoHumano" para encapsular los atributos
+ * de un recurso humano de los proyectos.
+ * ---------------------------------------------------------------------------------------------
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAPS.Entidades
+{
+    public class RecursoHumano
+    { 
+        private string m_usuario;
+        private string m_nombre;
+        private string m_correo;
+        private string m_telefono;
+        private string m_proyecto_asociado;
+        private string m_contrasena;
+        private bool m_es_administrador;
+
+        public RecursoHumano(Object[] datos) {
+
+        }
+
+        public string usuario
+        {
+            get { return m_usuario; }
+            set { m_usuario = value; }
+        }
+
+        public string nombre
+        {
+            get { return m_nombre; }
+            set { m_nombre = value; }
+        }
+
+        public string correo
+        {
+            get { return m_correo; }
+            set { m_correo = value; }
+        }
+
+        public string telefono
+        {
+            get { return m_telefono; }
+            set { m_telefono = value; }
+        }
+
+        public string proyecto_asociado
+        {
+            get { return m_proyecto_asociado; }
+            set { m_proyecto_asociado = value; }
+        }
+
+        public string contrasena
+        {
+            get { return m_contrasena; }
+            set { m_contrasena = value; }
+        }
+
+        public bool es_administrador
+        {
+            get { return m_es_administrador; }
+            set { m_es_administrador = value; }
+        }
+    }
+}
---
+++ SAPS/SAPS/Codigo_Fuente/Fronteras/Recursos_Humanos.aspx.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Universidad de Costa Rica
+ * Escuela de Ciencias de la Computación e Informática
+ * Ingeniería de Software I
+ * Sistema Administrador de Proyectos de Software (SAPS)
+ * II Semestre 2015
+ * --------------------------------------------------------------------------------------------
+ * Esta clase frontera se encarga de obtener los datos y los eventos que el usuario selecciona,
+ * se lo pasa a la controladora de la clase recurso_humano.
+ * --------------------------------------------------------------------------------------------
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SAPS.Fronteras
+{
+    public partial class Recursos_Humanos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            llena_recursos_humanos();
+
+        }
+
+        private void btn_lista_click(object sender, EventArgs e)
+        {
+            activa_botones_ime();
+        }
+
+        protected void btn_Cancelar_Click(object sender, EventArgs e)
+        {
+            limpia_campos();
+        }
+
+        protected void btn_consultar_Click(object sender, EventArgs e)
+        {
+            activa_botones_ime();
+        }
+
+        //Metodos auxiliares de la calse
+        private void activa_botones_ime()
+        {
+            btn_eliminar.Enabled = true;
+            btn_modificar.Enabled = true;
+        }
+
+        private void limpia_campos()
+        {
+            radio_btn_miembro.Checked = false;
+            radio_btn_administrador.Checked = false;
+            input_name.Text = "";
+            input_correo.Text = "";
+            input_telefono.Text = "";
+            input_usuario.Text = "";
+            input_contrasena.Text = "";
+        }
+
+        private void llena_recursos_humanos()
+        {
+            // Para llenar la tabla "tabla_consultas" dinámicamente
+            for (int i = 0; i < 4; ++i)
+            {
+                Button btn = new Button();
+                btn.ID = "btn_lista_" + i;
+                btn.Text = "RH " + i;
+                btn.Click += new EventHandler(btn_lista_click);
+                btn.CssClass = "list-group-item col-md-8";
+                lista_rh.Controls.AddAt(i, btn);
+            }
+        }
+    }
+}
