40807a55054df1a0ea74356de29525b5db37c3e6
SlyJose <jpurena14@hotmail.com>
2015-10-09 11:31:54 -0700

    Continuación Frontera PDP
    Se agregan nuevos espacios de texto a la interfaz.

--- SAPS/SAPS/Codigo_Fuente/Fronteras/InterfazProyectosDePruebas.aspx.cs
+++ SAPS/SAPS/Codigo_Fuente/Fronteras/InterfazProyectosDePruebas.aspx.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿/*
+ * Universidad de Costa Rica
+ * Escuela de Ciencias de la Computación e Informática
+ * Ingeniería de Software I
+ * Sistema Administrador de Proyectos de Software (SAPS)
+ * II Semestre 2015
+ * --------------------------------------------------------------------------------------------
+ * Esta clase frontera se encarga de obtener los datos y los eventos que el usuario selecciona,
+ * se lo pasa a la controladora de la clase proyecto_de_pruebas.
+ * --------------------------------------------------------------------------------------------
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SAPS.Codigo_Fuente.Controladoras;
+using System.Text.RegularExpressions;
 
-namespace SAPS.Codigo_Fuente.Fronteras
+namespace SAPS.Fronteras
 {
     public partial class InterfazProyectosDePruebas : System.Web.UI.Page
     {
+        ControladoraProyectoPruebas m_controladora_pdp;
         protected void Page_Load(object sender, EventArgs e)
         {
 
