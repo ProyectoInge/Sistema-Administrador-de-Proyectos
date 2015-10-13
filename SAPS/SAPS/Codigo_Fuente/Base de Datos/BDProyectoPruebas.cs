9581636403adfccb1dfc4c15730e809c51437525
carlosmataguzman <carlos.mataguzman@ucr.ac.cr>
2015-10-13 11:12:40 -0600

    Cambios en PyP

---
+++ SAPS/SAPS/Codigo_Fuente/Base de Datos/BDProyectoPruebas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAPS.Base_de_Datos
+{
+    public class BDProyectoPruebas
+    {
+    }
+}
--- SAPS/SAPS/Codigo_Fuente/Controladoras/ControladoraProyectoPruebas.cs
+++ SAPS/SAPS/Codigo_Fuente/Controladoras/ControladoraProyectoPruebas.cs
@@ -4,43 +4,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using SAPS.Entidades;
+using SAPS.Base_de_Datos;
+using SAPS.Ayudantes;
+
 
 namespace SAPS.Controladoras
 {
     public class ControladoraProyectoPruebas
     {
         //Variables de instancia
-        ProyectoPruebas m_entidad_proyecto;
+        BDProyectoPruebas m_base_datos;
         //Constructor
         public ControladoraProyectoPruebas()
         {
-
+            m_base_datos = new BDProyectoPruebas();
         }
+
+        // Métodos
+
+        /** @brief Método que asigna las operaciones necesarias para poder insertar un proyecto de pruebas.
+         * @param datos array que contiene los datos para poder insertar un proyecto de pruebas.
+         * @return 0 si la operación se realizó con éxito, números negativos si pasó algún error con la Base de Datos.
+         */
         public int insertar_proyecto(Object[] datos)
         {
-            return 0;
+            ProyectoPruebas proyecto = new ProyectoPruebas(datos);
+            return m_base_datos.insertar_proyecto(proyecto);
         }
 
+        /** @brief Método que asigna las operaciones necesarias para poder modificar un proyecto de pruebas.
+         * @param datos array que contiene los datos para poder crear un proyecto de pruebas.
+         * @return 0 si la operación se realizó con éxito, números negativos si pasó algún error con la Base de Datos.
+         */
         public int modificar_proyecto(Object[] datos)
         {
-            return 0;
+            ProyectoPruebas proyecto = new ProyectoPruebas(datos);
+            return m_base_datos.modificar_proyecto(proyecto);
         }
 
-        public int eliminar_proyecto(string nombre_pryecto)
+        /** @brief Método que asigna las operaciones necesarias para poder eliminar un proyecto de pruebas.
+         * @param string con el nombre del proyecto que se desea eliminar.
+         * @return 0 si la operación se realizó con éxito, números negativos si pasó algún error con la Base de Datos.
+         */
+        public int eliminar_proyecto(string nombre_proyecto)
         {
-            return 0;
+            return m_base_datos.eliminar_proyecto(nombre_proyecto);
         }
 
+        /** @brief Método que asigna las operaciones necesarias para poder consultar un proyecto de pruebas en específico.
+         * @param string con el nombre del proyecto que se desea cosultar.
+         * @return DataTable con los resultados de la consultas.
+         */
         public DataTable consultar_proyecto(string nombre_proyecto)
         {
-
-            return null;
+            return m_base_datos.consultar_proyecto(nombre_proyecto);
         }
 
+        /** @brief Método que asigna las operaciones necesarias para poder consultar los resucursos humanos dispinibles.
+         * @return DataTable con los resultados de la consultas.
+         */
         public DataTable solicitar_recursos_disponibles()
         {
-            return null;
-        }
+            ControladoraRecursosHumanos controladora_rh = new ControladoraRecursosHumanos();
+            return controladora_rh.solicitar_recursos_disponibles();
 
+        }
     }
 }
+
