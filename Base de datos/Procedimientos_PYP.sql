c4d99fae94574f7c191a644fd4bcbb9247ff1f20
SDelVecchioC <sdelvecchioc@gmail.com>
2015-10-14 13:33:14 +0800

    Se agregan los procs de RH y PYP

---
+++ Base de datos/Procedimientos_PYP.sql
@@ -0,0 +1,54 @@
+use proyectoDB
+
+DROP PROCEDURE INSERTAR_PYP
+DROP PROCEDURE MODIFICAR_PYP
+DROP PROCEDURE ELIMINAR_PYP
+DROP PROCEDURE CONSULTAR_PYP
+
+
+GO
+CREATE PROCEDURE INSERTAR_PYP --TO DO TOMAR EN CUENTA EL BIT ELIMINADO
+	@id_oficina varchar(32), @fecha_inicio date, @fecha_asignacion date, @fecha_final date, @nombre_sistema varchar(64), @obj_general varchar(256), @nombre_proyecto varchar(64), @estado varchar(32)
+AS
+	INSERT INTO ProyectoPruebas
+		(id_oficina, fecha_inicio, fecha_asignacion, fecha_final, nombre_sistema, obj_general, nombre_proyecto, estado)
+	VALUES
+		(@id_oficina, @fecha_inicio, @fecha_asignacion, @fecha_final, @nombre_sistema, @obj_general, @nombre_proyecto, @estado)
+GO
+
+GO
+CREATE PROCEDURE MODIFICAR_PYP
+	@id_proyecto int, @id_oficina varchar(32), @fecha_inicio date, @fecha_asignacion date, @fecha_final date, @nombre_sistema varchar(64), @obj_general varchar(256), @nombre_proyecto varchar(64), @estado varchar(32)
+AS
+	UPDATE ProyectoPruebas
+		SET id_oficina = @id_oficina, fecha_inicio = @fecha_inicio, fecha_asignacion=@fecha_asignacion, fecha_final=@fecha_final, nombre_sistema=@nombre_sistema, obj_general=@obj_general, nombre_proyecto=@nombre_proyecto, estado = @estado
+		WHERE id_proyecto = @id_proyecto
+GO
+
+
+GO
+CREATE PROCEDURE ELIMINAR_PYP
+	@id_proyecto int
+AS
+	UPDATE ProyectoPruebas
+		SET eliminado = 1
+		WHERE id_proyecto = @id_proyecto
+GO
+
+
+GO
+CREATE PROCEDURE CONSULTAR_PYP
+	@id_proyecto int
+AS
+	SELECT	ProyectoPruebas.id_oficina,
+			ProyectoPruebas.fecha_inicio,
+			ProyectoPruebas.fecha_asignacion,
+			ProyectoPruebas.fecha_final,
+			ProyectoPruebas.nombre_sistema,
+			ProyectoPruebas.obj_general,
+			ProyectoPruebas.nombre_proyecto,
+			ProyectoPruebas.estado,
+			ProyectoPruebas.eliminado
+	FROM	ProyectoPruebas
+	WHERE	ProyectoPruebas.id_proyecto = @id_proyecto AND ProyectoPruebas.eliminado = 0
+			
