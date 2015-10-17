09a28a04b231c18da3bb0141f0e153b54a83ed47
carlosmataguzman <carlos.mataguzman@ucr.ac.cr>
2015-10-17 00:17:06 -0600

    Se agrega procedimiento almacenado insertar oficina

--- Base de datos/Procedimientos_PYP.sql
+++ Base de datos/Procedimientos_PYP.sql
@@ -6,6 +6,7 @@ DROP PROCEDURE ELIMINAR_PYP
 DROP PROCEDURE CONSULTAR_PYP
 DROP PROCEDURE CONSULTAR_PROYECTOS_DISPONIBLES
 DROP PROCEDURE CONSULTAR_OFICINAS_DISPONIBLES
+DROP PROCEDURE INSERTAR_OFICINA
 
 
 GO
@@ -78,3 +79,18 @@ AS BEGIN
 	FROM	Oficina
 	END
 GO
+
+
+GO
+CREATE PROCEDURE INSERTAR_OFICINA
+	@nombre_oficina varchar(64), @telefono varchar(16), @telefono2 varchar(16), @nom_representante varchar(64)
+AS
+	INSERT INTO Oficina
+		(nombre_oficina, telefono, telefono2, nom_representante)
+	VALUES
+		(@nombre_oficina, @telefono, @telefono2, @nom_representante) 
+GO
+
+execute INSERTAR_OFICINA 'Oficina11', '25110000','256987541', 'alguien'
+
+
