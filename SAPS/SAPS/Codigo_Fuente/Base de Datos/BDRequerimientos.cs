bd1c616b13915999d20b1eacf2a20d2325d8b4e1
Fabian Rodriguez <farodriguez.49@gmail.com>
2015-10-30 21:04:35 -0600

    Se crean los procedimientos almacenados para los Requerimientos

---
+++ SAPS/SAPS/Codigo_Fuente/Base de Datos/BDRequerimientos.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Universidad de Costa Rica
+ * Escuela de Ciencias de la Computación e Informática
+ * Ingeniería de Software I
+ * Sistema Administrador de Proyectos de Software (SAPS)
+ * II Semestre 2015
+*/
+
+
+using SAPS.Base_de_Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAPS.Base_de_Datos
+{
+    /** @brief capa encargada de comunicarse con la base de datos para efectuar las correspondientes inserciones, modificaciones,
+               eliminaciones y consultas SQL relacionadas con los requerimientos.
+     */
+    public class BDRequerimientos
+    {
+        // Variables de instancia
+        DataBaseAdapter m_data_base_adapter;
+
+        //Constructor
+        public BDRequerimientos()
+        {
+            m_data_base_adapter = new DataBaseAdapter();
+        }
+
+
+    }
+}
