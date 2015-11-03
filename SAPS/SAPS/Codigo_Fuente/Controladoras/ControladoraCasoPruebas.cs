975149c6528ee73b472a791a93ede98ca62b28a2
SlyJose <jpurena14@hotmail.com>
2015-11-03 10:27:21 -0600

    Declaración de todos los métodos en la controladora de Casos de Pruebas.

---
+++ SAPS/SAPS/Codigo_Fuente/Controladoras/ControladoraCasoPruebas.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Universidad de Costa Rica
+ * Escuela de Ciencias de la Computación e Informática
+ * Ingeniería de Software I
+ * Sistema Administrador de Proyectos de Software (SAPS)
+ * II Semestre 2015
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SAPS.Base_de_Datos;
+using SAPS.Entidades;
+using System.Data;
+
+namespace SAPS.Codigo_Fuente.Controladoras
+{
+    /** @brief Efectuar las comunicaciones relacionadas con los casos de pruebas con la capa de
+               interfaz, la capa de  base de datos y controladoras de otros módulos.
+    */
+    public class ControladoraCasoPruebas
+    {
+        private BDCasoPruebas m_base_datos;
+
+        // Constructor de la clase
+        public ControladoraCasoPruebas()
+        {
+            m_base_datos = new BDCasoPruebas();
+        }
+
+        /** @brief Método que se encarga de las operaciones necesarias para insertar un nuevo caso de pruebas en la base de datos.
+        * @param Vector de "objects" con los datos para el nuevo caso de pruebas.
+        * @return 0 si tuvo éxito, números negativos si se presentó un error con la base de datos.
+        */
+        public int insertar_caso_pruebas(Object[] datos)
+        {
+
+        }
+
+        /** @brief Método que se encarga de las operaciones necesarias para modificar un caso de pruebas en la base de datos.
+        * @param Vector de "objects" con los datos para el caso de pruebas.
+        * @return 0 si tuvo éxito, números negativos si se presentó un error con la base de datos.
+        */
+        public int modificar_caso_pruebas(Object[] datos)
+        {
+
+        }
+
+        /** @brief Método que se encarga de las operaciones necesarias para eliminar un nuevo caso de pruebas en la base de datos.
+        * @param ID del caso que se desea eliminar.
+        * @return 0 si tuvo éxito, números negativos si se presentó un error con la base de datos.
+        */
+        public int eliminar_caso_pruebas(int id_diseno)
+        {
+
+        }
+
+        /** @brief Método que se encarga de las operaciones necesarias para consultar un nuevo caso de pruebas en específico.
+        * @param ID del caso que se desea consultar.
+        * @return DataTable con los datos del caso de pruebas que se consultó.
+        */
+        public DataTable consultar_caso_pruebas(int id_diseno)
+        {
+
+        }
+
+        /** @brief Método que se encarga de las operaciones necesarias para consultar los casos de pruebas disponibles que hay en la base de datos.
+        * @return DataTable con los datos de todos los casos de pruebas disponibles que hay en la base de datos.
+        */
+        public DataTable solicitar_disenos_disponibles() {
+
+        }
+
+
+    }
+}
