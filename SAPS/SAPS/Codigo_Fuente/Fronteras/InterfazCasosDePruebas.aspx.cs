66cdf9dd7a1e7d789218f3b2b4355db8992cf0dd
carlosmataguzman <carlos.mataguzman@ucr.ac.cr>
2015-11-04 13:04:32 -0600

    Inicio de creación de la interfaz

--- SAPS/SAPS/Codigo_Fuente/Fronteras/InterfazCasosDePruebas.aspx.cs
+++ SAPS/SAPS/Codigo_Fuente/Fronteras/InterfazCasosDePruebas.aspx.cs
@@ -13,5 +13,80 @@ namespace SAPS.Fronteras
         {
 
         }
+
+        /** @brief Evento que se activa cuando el usuario selecciona el boton de "cancelar".
+         * @param Los parametros por default de un evento de C#.
+        */
+        protected void btn_cancelar_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        /** @brief Evento que se activa cuando el usuario selecciona el boton de "aceptar".
+        * @param Los parametros por default de un evento de C#.
+        */
+        protected void btn_aceptar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /** @brief Evento que se activa cuando el usuario selecciona el boton de "crear".
+        * @param Los parametros por default de un evento de C#.
+        */
+        protected void btn_crear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /** @brief Evento que se activa cuando el usuario selecciona el boton de "modificar".
+        * @param Los parametros por default de un evento de C#.
+        */
+        protected void btn_modificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /** @brief Evento que se activa cuando el usuario selecciona el boton de "eliminar".
+       * @param Los parametros por default de un evento de C#.
+       */
+        protected void btn_eliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /** @brief Evento que se activa cuando el usuario selecciona el boton de "consultar".
+         * @param Los parametros por default de un evento de C#.
+        */
+        protected void btn_consultar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /** @brief Metodo que se activa cuando el usuario selecciona un proyecto del dropdown, llena la informacion correspondiente a ese proyecto.
+         * @param Los parametros por default de un evento de C#.
+        */
+        protected void drop_proyecto_asociado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        /** @brief Metodo que se activa cuando el usuario selecciona un diseño del dropdown, llena la informacion correspondiente a ese diseño.
+         * @param Los parametros por default de un evento de C#.
+        */
+        protected void drop_diseno_asociado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /** @brief Metodo que se activa cuando el usuario selecciona un diseño del dropdown, llena la informacion correspondiente a las características ese diseño.
+        * @param Los parametros por default de un evento de C#.
+        */
+        protected void drop_caracteristicas_diseno_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
     }
 }
--- SAPS/SAPS/Codigo_Fuente/Fronteras/InterfazCasosDePruebas.aspx.designer.cs
+++ SAPS/SAPS/Codigo_Fuente/Fronteras/InterfazCasosDePruebas.aspx.designer.cs
@@ -11,5 +11,176 @@ namespace SAPS.Fronteras {
     
     
     public partial class InterfazCasosDePruebas {
+        
+        /// <summary>
+        /// Control btn_crear.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_crear;
+        
+        /// <summary>
+        /// Control btn_modificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_modificar;
+        
+        /// <summary>
+        /// Control btn_eliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_eliminar;
+        
+        /// <summary>
+        /// Control alerta_error.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl alerta_error;
+        
+        /// <summary>
+        /// Control cuerpo_alerta_error.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label cuerpo_alerta_error;
+        
+        /// <summary>
+        /// Control alerta_exito.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl alerta_exito;
+        
+        /// <summary>
+        /// Control cuerpo_alerta_exito.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label cuerpo_alerta_exito;
+        
+        /// <summary>
+        /// Control alerta_advertencia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl alerta_advertencia;
+        
+        /// <summary>
+        /// Control cuerpo_alerta_advertencia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label cuerpo_alerta_advertencia;
+        
+        /// <summary>
+        /// Control label_proyecto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label_proyecto;
+        
+        /// <summary>
+        /// Control drop_proyecto_asociado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList drop_proyecto_asociado;
+        
+        /// <summary>
+        /// Control label_diseno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label_diseno;
+        
+        /// <summary>
+        /// Control drop_diseno_asociado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList drop_diseno_asociado;
+        
+        /// <summary>
+        /// Control label_caracteristicas_diseno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label_caracteristicas_diseno;
+        
+        /// <summary>
+        /// Control drop_caracteristicas_diseno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList drop_caracteristicas_diseno;
+        
+        /// <summary>
+        /// Control btn_Aceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Aceptar;
+        
+        /// <summary>
+        /// Control btn_Cancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Cancelar;
+        
+        /// <summary>
+        /// Control tabla_casos_pruebas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table tabla_casos_pruebas;
+        
+        /// <summary>
+        /// Control tabla_casos_pruebas_header.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableHeaderRow tabla_casos_pruebas_header;
     }
 }
