2e8e1bb02ac717e6d025d9bd464d7b76bf3eb4a5
SlyJose <jpurena14@hotmail.com>
2015-11-18 10:45:08 -0600

    Creacion e inicio de entidades EP y REP

---
+++ SAPS/SAPS/Codigo_Fuente/Entidades/EjecucionPruebas.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Universidad de Costa Rica
+ * Escuela de Ciencias de la Computación e Informática
+ * Ingeniería de Software I
+ * Sistema Administrador de Proyectos de Software (SAPS)
+ * II Semestre 2015
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAPS.Codigo_Fuente.Entidades
+{
+    public class EjecucionPruebas
+    {
+        // Elementos de ejecucion de pruebas
+
+        private int numero_ejecucion;
+        private string responsable;
+        private int id_diseno;
+        private DateTime fecha_ultima_ejecucion;
+        private string incidencias;
+
+                /* @brief Constructor de la entidad de ejecucion de pruebas.
+                @param datos contiene los datos para poder crear una Ejecucion de pruebas, el orden va de la siguiente manera:
+                                     * | Índice | Descripción             | Tipo de datos |
+                                     * |:------:|:-----------------------:|:-------------:|
+                                     * |    0   |  Numero de ejecucion    |      int      |
+                                     * |    1   |  Responsable            |     String    |
+                                     * |    2   |  Id del diseno          |      int      |
+                                     * |    3   |  Fecha de ejecucion     |     Datetime  |
+                                     * |    4   |  Incidencias            |     String    |
+                * **/         
+
+    }
+}
+ 
