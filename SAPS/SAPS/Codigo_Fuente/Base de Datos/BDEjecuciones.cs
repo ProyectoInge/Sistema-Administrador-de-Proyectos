7f4c4549d61dc0e15ba92b8cc93aabc7fc725297
SDelVecchioC <sdelvecchioc@gmail.com>
2015-11-18 16:36:12 -0600

    Se agregan los metodos de eliminar EP y se crean la cont de BD y la cont.

---
+++ SAPS/SAPS/Codigo_Fuente/Base de Datos/BDEjecuciones.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SAPS.Base_de_Datos
+{
+    public class BDEjecuciones
+    {
+        /// Variables de instancia
+        private DataBaseAdapter m_data_base_adapter;
+
+        ///@brief Constructor
+        public BDEjecuciones()
+        {
+            m_data_base_adapter = new DataBaseAdapter();
+        }
+
+        /** @brief Método que realiza la setencia SQL para eliminar una ejecución en específico.
+         * @param num de la ejecución que se desea eliminar y el id de diseño relacionado.
+         * @return 0 si la operación se realizó con éxito, números negativos si pasó algún error con la Base de Datos.
+         */
+        internal int eliminar_requerimiento(int id_diseno, int num_ejecucion)
+        {
+            SqlCommand comando = new SqlCommand("ELIMINAR_EP");
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.Add("@num_ejecucion", SqlDbType.Int).Value = num_ejecucion;
+            comando.Parameters.Add("@id_diseno", SqlDbType.Int).Value = id_diseno;
+            return m_data_base_adapter.ejecutar_consulta(comando);
+        }
+    }
+}
---
+++ SAPS/SAPS/Codigo_Fuente/Controladoras/ControladoraEjecuciones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SAPS.Base_de_Datos;
+
+namespace SAPS.Codigo_Fuente.Controladoras
+{
+    public class ControladoraEjecuciones
+    {
+        /// Variables de instancia
+        private BDEjecuciones m_base_datos;
+
+        ///@brief Constructor
+        public ControladoraEjecuciones()
+        {
+            m_base_datos = new BDEjecuciones();
+        }
+
+        /** @brief Método que se encarga de las operaciones necesarias para eliminar una ejecucion de pruebas de la base de datos.
+        * @param num de la ejecución que se desea eliminar y el id de diseño relacionado.
+        * @return 0 si tuvo éxito, números negativos si se presentó un error con la base de datos.
+        */
+        public int eliminar_diseno_pruebas(int id_diseno, int num_ejecucion)
+        {
+            return m_base_datos.eliminar_requerimiento(id_diseno, num_ejecucion);
+        }
+    }
+}
