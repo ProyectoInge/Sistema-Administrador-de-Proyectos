0676549a98b9371ed85223f16c0c2d1fc0ae764c
SlyJose <jpurena14@hotmail.com>
2015-11-18 11:29:35 -0600

    Desarrollo entidad resultados de caso de pruebas, es necesario averiguar como manejar la imagen.

--- SAPS/SAPS/Codigo_Fuente/Entidades/Resultados_EP.cs
+++ SAPS/SAPS/Codigo_Fuente/Entidades/Resultados_EP.cs
@@ -7,5 +7,42 @@ namespace SAPS.Codigo_Fuente.Entidades
 {
     public class Resultados_EP
     {
+        private int numero_resultado;
+        private int id_diseno;
+        private int numero_ejecucion;
+        private string estado;
+        private string tipo_no_conformidad;
+        private string id_caso;
+        private string desc_no_conformidad;
+        private string justificacion;
+        // variable que maneja la imagen, falta averiguar como ponerlo
+
+        /* @brief Constructor de la entidad de resultados de ejecucion de pruebas.
+        @param datos contiene los datos para poder crear un conjunto asociado a una ejecucion de pruebas, el orden va de la siguiente manera:
+                             * | Índice | Descripción             | Tipo de datos |
+                             * |:------:|:-----------------------:|:-------------:|
+                             * |    0   |  Numero de resultado    |      int      |
+                             * |    1   |  Id del diseno          |      int      |
+                             * |    2   |  Numero de ejecucion    |      int      |
+                             * |    3   |  Estado                 |     String    |
+                             * |    4   |  Tipo No Conformidad    |     String    |
+                             * |    5   |  Id del Caso            |     String    |
+                             * |    6   |  Descripcion No Conf.   |     String    |
+                             * |    7   |  Justificacion          |     String    |
+                             * |    8   |  Imagen                 |     Averiguar |
+        * **/
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
