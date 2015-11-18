d8f62db253e8cd67b6ebbc447999457782ba12b5
SlyJose <jpurena14@hotmail.com>
2015-11-18 11:07:44 -0600

    Se finaliza la entidad de ejecucion de pruebas.

--- SAPS/SAPS/Codigo_Fuente/Entidades/EjecucionPruebas.cs
+++ SAPS/SAPS/Codigo_Fuente/Entidades/EjecucionPruebas.cs
@@ -24,17 +24,65 @@ namespace SAPS.Codigo_Fuente.Entidades
         private DateTime fecha_ultima_ejecucion;
         private string incidencias;
 
-                /* @brief Constructor de la entidad de ejecucion de pruebas.
-                @param datos contiene los datos para poder crear una Ejecucion de pruebas, el orden va de la siguiente manera:
-                                     * | Índice | Descripción             | Tipo de datos |
-                                     * |:------:|:-----------------------:|:-------------:|
-                                     * |    0   |  Numero de ejecucion    |      int      |
-                                     * |    1   |  Responsable            |     String    |
-                                     * |    2   |  Id del diseno          |      int      |
-                                     * |    3   |  Fecha de ejecucion     |     Datetime  |
-                                     * |    4   |  Incidencias            |     String    |
-                * **/         
+        /* @brief Constructor de la entidad de ejecucion de pruebas.
+        @param datos contiene los datos para poder crear una Ejecucion de pruebas, el orden va de la siguiente manera:
+                             * | Índice | Descripción             | Tipo de datos |
+                             * |:------:|:-----------------------:|:-------------:|
+                             * |    0   |  Numero de ejecucion    |      int      |
+                             * |    1   |  Responsable            |     String    |
+                             * |    2   |  Id del diseno          |      int      |
+                             * |    3   |  Fecha de ejecucion     |     Datetime  |
+                             * |    4   |  Incidencias            |     String    |
+        * **/
+        public EjecucionPruebas(Object[] datos)
+        {
+            numero_ejecucion = Convert.ToInt32(datos[0]);
+            responsable = datos[1].ToString();
+            id_diseno = Convert.ToInt32(datos[2]);
+            fecha_ultima_ejecucion = Convert.ToDateTime(datos[3]);
+            incidencias = datos[4].ToString();
+        }
 
+        public int n_ejecucion
+        {            
+            get{ return numero_ejecucion;  }
+            set { numero_ejecucion = value; }
+        }
+
+        public string ep_responsable
+        {
+            get { return responsable; }
+            set { responsable = value; }
+        }
+
+        public int diseno_asociado
+        {
+            get { return id_diseno; }
+            set { id_diseno = value; }
+        }
+
+        public DateTime fecha_ejecucion
+        {
+            get { return fecha_ultima_ejecucion; }
+            set { fecha_ultima_ejecucion = value; }
+        }
+
+        public string incidencias_ep
+        {
+            get { return incidencias; }
+            set { incidencias = value; }
+        }
     }
 }
  
+
+
+
+
+
+
+
+
+
+
+
