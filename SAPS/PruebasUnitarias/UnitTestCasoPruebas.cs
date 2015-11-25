47801b834e3b3663ef8d405c56ce6ef6151cc13a
SlyJose <jpurena14@hotmail.com>
2015-11-25 10:58:30 -0600

    Continuación prueba unit test para casos de pruebas.

--- SAPS/PruebasUnitarias/UnitTestCasoPruebas.cs
+++ SAPS/PruebasUnitarias/UnitTestCasoPruebas.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SAPS.Controladoras;
+using SAPS.Entidades.Ayudantes;
 
 namespace PruebasUnitarias
 {
@@ -17,7 +18,24 @@ namespace PruebasUnitarias
         **/
         public void test_insertar_correcto()
         {
-            
+            Object[] datos = { "id_caso", "diseno", "id_requerimiento", "proposito", "resultados", "flujo" };
+            Dato[] entrada_datos = new Dato[2];
+
+            entrada_datos[0] = new Dato("Valor", "Valido"); 
+            entrada_datos[1] = new Dato("Valor2", "InValido");
+
+            int resultado = m_controladora_cp.insertar_caso_pruebas(datos, entrada_datos);
+            int esperado = 0;
+
+            Assert.AreEqual(resultado, esperado);
+        }
+
+        [TestMethod]
+        /**@brief Metodo que evalua si un caso de prueba con los datos incorrectos falla
+        **/
+        public void test_insertar_incorrecto()
+        {
+
         }
 
 
