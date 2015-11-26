92204b349190d582bee2b73eae83ade69367e3db
carlosmataguzman <carlos.mataguzman@ucr.ac.cr>
2015-11-26 11:45:03 -0600

    Filtros en pruebas l

--- Base de datos/Procedimientos_Reportes.sql
+++ Base de datos/Procedimientos_Reportes.sql
@@ -15,15 +15,15 @@ SET @SQLCommand = 'SELECT *FROM ProyectoPruebas WHERE ' --los espacios importan
 IF @filtro_id_oficina != ''
 SET @SQLCommand = @SQLCommand+char(39)+@filtro_id_oficina+char(39)+' = ProyectoPruebas.id_oficina AND ';	
 IF @filtro_despues_de != ''
-SET @SQLCommand = @SQLCommand+Convert(varchar(16),@filtro_despues_de,10)+' <= ProyectoPruebas.fecha_inicio AND '; --datetime styles : http://www.w3schools.com/sql/func_convert.asp
+SET @SQLCommand = @SQLCommand+'Cast('+char(39)+Convert(varchar(16),@filtro_despues_de,10)+char(39)+' as datetime) <= ProyectoPruebas.fecha_inicio AND '; 
 IF @filtro_antes_de != ''
-SET @SQLCommand = @SQLCommand+Convert(varchar(16),@filtro_antes_de,10)+' <= ProyectoPruebas.fecha_final AND ';
+SET @SQLCommand = @SQLCommand+'Cast('+char(39)+Convert(varchar(16),@filtro_antes_de,10)+char(39)+' as datetime) <= ProyectoPruebas.fecha_final AND ';
 IF @filtro_nombre_sistema != ''
 SET @SQLCommand = @SQLCommand+char(39)+@filtro_nombre_sistema+char(39)+' = ProyectoPruebas.nombre_sistema AND ';
 IF @filtro_estado != ''
 SET @SQLCommand = @SQLCommand+char(39)+@filtro_estado+char(39)+' = ProyectoPruebas.estado AND';
 SET @SQLCommand = @SQLCommand+' 0=0 ';  --se podrá mejorar?
-PRINT(@SQLCommand)
+EXECUTE(@SQLCommand)
 GO
 
 execute FILTRAR_PROYECTOS '', '', '', '',''						--Sin filtro
@@ -32,12 +32,6 @@ execute FILTRAR_PROYECTOS '1', '', '', 'HIP HOP','Finalizado'
 
 
 DECLARE @date1 datetime, @date2 datetime
-SET @date1= '09-30-2015'
-SET @date2= '09-02-2015'
-execute FILTRAR_PROYECTOS '', @date1, '', '',''			--Filtro entre dos fechas
-
-execute FILTRAR_PROYECTOS '', '', '', '',''						--Sin filtro
-
-
-
-SELECT *FROM ProyectoPruebas WHERE 09-30-2015 < ProyectoPruebas.fecha_inicio AND  0=0
+SET @date1= '08-30-2015'
+SET @date2= '12-10-2015'
+execute FILTRAR_PROYECTOS '', @date1, @date2, '',''			--Filtro entre dos fechas
